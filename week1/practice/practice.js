a2971b2db565302daa6caa96574a740f7f737fc4
NicolasPariente <par21022@byui.edu>
2022-01-25 14:45:14 -0300

    using local storage

--- week1/practice/practice.js
+++ week1/practice/practice.js
@@ -4,3 +4,11 @@ function changeblue() {
 function changegreen() {
   document.body.style.backgroundColor = "green";
 }
+function save() {
+  let text = document.querySelector("#textarea").value;
+  localStorage.setItem("user", text);
+}
+function publish() {
+  let publish = localStorage.getItem("user");
+  document.querySelector("#publish").innerHTML = publish;
+}
